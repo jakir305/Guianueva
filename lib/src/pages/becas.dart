cc96bb63d8d1ec01280bdcd64c7640e0eb4afef3
jakiro305 <64810161+jakiro305@users.noreply.github.com>
2020-07-10 15:48:30 -0300

    Becas Completas
    Esta hecho la parte de becas, para pedir diseños. Esta agregada 1 sola beca, para esperar corrección

--- lib/src/pages/becas.dart
+++ lib/src/pages/becas.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:guiae/src/widgets/cards_becas.dart';
 
-class Becasvarias extends StatefulWidget {
-  @override
-  _BecasvariasState createState() => _BecasvariasState();
-}
+class Becasvarias extends StatelessWidget {
 
-class _BecasvariasState extends State<Becasvarias> {
-  @override
+ @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        title: Text("Becas")
-      ),
-      body: ListView(
-        children: <Widget>[
-
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Becas'),
+          backgroundColor: Colors.tealAccent[700],
+          elevation: 0.0,
+          
+        ),
         
-        
-        ],
-      ),
-      
+          body: Container(
+          padding: EdgeInsets.all(10),
+          child: _becasCard()),
+      )
     );
+  }
+  
+   Widget _becasCard() {
+      return ListView(
+        children: <Widget>[
+                ListCardsBecas(
+                  institucion: 'Universidad Nacional de Cuyo',
+                  nombre: "Beca Economica",
+                  icon: AssetImage('Asset/unco.png'),
+                  color: Colors.blueAccent,
+                  url: "http://www.uncuyo.edu.ar/bienestar/oferta-de-becas",  
+       ),
+        ]
+      );
+
   }
 }
---
+++ lib/src/widgets/cards_becas.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ListCardsBecas extends StatelessWidget {
+  
+  final String institucion;
+  final String nombre;
+  final AssetImage icon;
+  final Color color;
+  final String url;
+
+  ListCardsBecas({
+    @required this.institucion,
+    this.nombre,
+    this.icon,
+    this.color,
+    this.url,
+    });
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+        color: Colors.white,
+        elevation: 100.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        child: InkWell(
+            onTap: () async{
+       launch(url);
+       },
+            child: Row(
+            children: <Widget>[
+              
+              AnimatedContainer(
+                duration: Duration(seconds: 1),
+                width  :100,
+                height :100,
+                child: 
+                Image(image: icon),
+              ),
+                SizedBox(width: 20.0,),
+             Column(
+               children: <Widget>[
+                 
+                 SizedBox(height: 20,),
+                 Text(nombre,
+                 style: TextStyle(
+                   fontSize: 20.0,
+                   color: color,
+                 ),
+                 ),
+
+                 SizedBox(height: 20,),
+                 Text(institucion,
+                 style:TextStyle(
+                  fontSize:15,
+                  color:Colors.black,),
+
+                 textAlign:TextAlign.center,),
+               ],
+             ),
+            ],),
+        ),
+      );
+  }
+}
+  
