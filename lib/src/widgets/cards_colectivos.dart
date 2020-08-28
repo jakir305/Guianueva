b1bd193a5d4be74d8da9724f626abd5ffe17981f
jakiro305 <64810161+jakiro305@users.noreply.github.com>
2020-08-28 13:40:11 -0300

    Colectivos

--- lib/src/widgets/cards_colectivos.dart
+++ lib/src/widgets/cards_colectivos.dart
@@ -1,45 +1,47 @@
-
 import 'package:flutter/material.dart';
-import 'package:guiae/src/Utils/text_style.dart';
-import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ListCardsColectivos extends StatelessWidget {
-  
   final String lugar;
   final String url;
   final Color color;
-
 
   ListCardsColectivos({
     @required this.lugar,
     this.url,
     this.color,
-    });
-
+  });
 
   @override
   Widget build(BuildContext context) {
-
     return Card(
-        color: Colors.white,
-        elevation: 10,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-        child: InkWell(
-            onTap:  () async {
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+      color: Colors.white,
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+        50.0,
+      )),
+      child: InkWell(
+        onTap: () async {
           launch(url);
         },
-            child: Row(
-            children: <Widget>[
-              Icon(LineAwesomeIcons.bus,
-              size: 70.0,
-              color: color),
-              SizedBox(width: 10.0,),
-              Text(lugar,style: styleNombreUniver,textAlign:TextAlign.center,),
-            
-            ],),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Color.fromRGBO(144, 71, 150, 1),
+              width: 2,
+            ),
+          ),
+          height: 70,
+          alignment: AlignmentDirectional.center,
+          child: Text(
+            lugar,
+            style: TextStyle(fontFamily: 'MMedium', fontSize: 30),
+            textAlign: TextAlign.center,
+          ),
         ),
-      );
+      ),
+    );
   }
-    }
-  
+}
